f0bd9072efca07d142fdaf135a4e3228c22cac0c
Peter Wyrd <thewyrdguy@gmail.com>
2025-05-07 19:07:40 +0200

    run.sh: allow file name

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-`dirname $0`/ecgtovideo -r 30 | ffmpeg -y -r 30 -f image2pipe -c:v png -i - -r 30 -an vid.mp4
+if [ ! -r "$1" ]; then
+       echo "no file $1"
+       exit 1
+fi
+
+vid=`echo "$1" | sed -e 's/txt$/mp4/'`
+
+echo "converting $1 to $vid"
+
+`dirname $0`/ecgtovideo -r 30 < "$1" | ffmpeg -y -r 30 -f image2pipe -c:v png -i - -r 30 -an "$vid"
